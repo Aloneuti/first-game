89f7ff52e599a27ff44ae0b7a7ac6dfaa17b894f
Lucas Morais <lucas.eredios@gmail.com>
2025-04-01 23:56:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,6 @@ import random
 
 app = FastAPI()
 
-# 127.0.0.1:8000/
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/test1")
-async def funcaoteste():
-    return {"message": True, "random_number": random.randint(0 , 1000)}
-
-@app.get("/test2")
-async def test():
-    return {"message": "number"}
+def home():
+    return "minha api está no ar"
