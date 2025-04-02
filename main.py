6559bcd9bdef3d423f2687eec84d41157bf90a64
Lucas Morais <lucas.eredios@gmail.com>
2025-04-02 18:28:18 -0300

    commit

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-from fastapi import FastAPI
 import random
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+
+class Player(BaseModel):
+    nickname: str
+    platform: str
+    Active: bool
+
 @app.get("/")
-def home():
+async def home():
     return {"message": "Aqui é o começo"}
 
 @app.get("/number")
-def number():
+async def number():
     return {"Number": True, "Random_number": random.randint(0,154)}
 
 @app.get("/game")
-def gamepage():
+async def gamepage():
     return {"Aqui será a pagina do game"}
+
+@app.post("/signup")
+async def signuppage(nickname: Player):
+    return nickname
+
+@app.put("/signup/update/{id_player}")
+async def update_player(id_player: int):
+    return id_player > 0
+
+@app.delete("up/delete/{id_player}")
+async def delete_player(id_player: int):
+    return id_player > 0
