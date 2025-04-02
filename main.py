f654344ae6e02aa560fc9a76a538e6a14bf42d6e
Lucas Morais <lucas.eredios@gmail.com>
2025-04-01 22:54:28 -0300

    configuração inicial
    test

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# 127.0.0.1:8000/
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/test1")
+async def funcaoteste():
+    return {"teste": "deu certo"}
