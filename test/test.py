2da1761fa2b6067748884f26ce8849bde787bebe
Lucas Morais <lucas.eredios@gmail.com>
2025-04-27 18:03:13 -0300

    test-test1

---
+++ test/test.py
@@ -0,0 +1,29 @@
+from src.main import *
+from unittest.mock import patch
+
+def test_home():
+    assert home() == {"message": "Aqui é o começo"}
+
+
+def test_number():
+    with patch('random.randint', return_value=12345):
+        result = number()
+
+    assert result== {"Number": True, "Random_number": 12345}
+
+def test_signuppage():
+    nickname_teste = Player(nickname="Fulano", platform="Steam", Active=False)
+    assert nickname_teste == signuppage()
+
+
+def test_update_player_negative():
+    assert not update_player(-5)
+
+def test_update_player_positive():
+    assert update_player(10)
+
+def test_delete_player_negative():
+    assert not delete_player(-5)
+
+def test_delete_player_positive():
+    assert not delete_player(5)
